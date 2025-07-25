030308504c119490a535ec88c921fb72134f1b2c
Revoj-a <revojjoever@gmail.com>
2025-07-25 22:51:17 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const container = document.querySelector("#container");
+
+const frontURL =
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+const backURL =
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/";
